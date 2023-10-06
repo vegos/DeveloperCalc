ecb6c595011347531bfa4e439c8b42258bdbe0b8
Antonis <maglaras@gmail.com>
2023-10-06 22:30:38 +0300

    v0.2

--- developtime.py
+++ developtime.py
@@ -5,22 +5,19 @@
 #
 import math
 print
-print "DEVELOPER TIME CALCULATOR"
-print "(c)2014, Antonis Maglaras"
+print ("DEVELOPER TIME CALCULATOR")
+print ("(c)2014, Antonis Maglaras")
 print
 
 # roundtime = 1 if you want to round time
 roundtime = 1
-# for C use -0.081
-# for F use -0.045
-constForExp = -0.081
 
-time_input=raw_input("Enter exact time for 20C (in MM:SS): ")
+time_input = input("Enter exact time for 20C (in MM:SS): ")
 InputMin, InputSec = [int(i) for i in time_input.split(':')]
 OldTime = InputMin*60 + InputSec
 
 NewTemp = input("New Temperature (in Celsius): ")
-NewTime = OldTime*math.exp(constForExp*(NewTemp-20))
+NewTime = int(OldTime)*math.exp(-0.081*(int(NewTemp)-20))
 NewMin, NewSec = divmod(NewTime, 60)
 
 if roundtime == 1:
@@ -38,11 +35,10 @@ if roundtime == 1:
   NewSec = tmpSec
 
 print 
-print "New Time for %dC: %02d:%02d" % (NewTemp, NewMin, NewSec)
+print ("New Time for %dC: %2d:%2d" % (int(NewTemp), int(NewMin), int(NewSec)))
 print
 
 if NewMin < 5:
-  print "Warning! Development times below 5 minutes are not recommended"
-  print "         due to the risk of uneven development!"
+  print ("Warning! Development times below 5 minutes are not recommended")
+  print ("         due to the risk of uneven development!")
   print
-
